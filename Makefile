f4ea3dd389fd4d79dc3ab40dbac77b763fc0843a
parsaM110 <pmomeni80@gmail.com>
2023-04-17 09:11:19 +0330

    adding lohmann_json

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
+dependency:
+	cd build && cmake .. --graphviz=graph.dot && dot -Tpng graph.dot -o graphImage.png
 perpare:
 	rmdir /s /q build
 	mkdir build
+
 
--- app/main.cc
+++ app/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<nlohmann/json.hpp>
 
 #include "my_lib.h"
 #include "config.hpp"
@@ -8,6 +9,12 @@ int main()
 {
     std::cout << project_name << "\n";
     std::cout<< project_version << "\n";
+
+    std::cout << "JSON Lib Version:"
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
     print_hello_world();
     return 0;
 }
